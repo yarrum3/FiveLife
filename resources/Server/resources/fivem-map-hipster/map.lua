0643a83c969cf9af0b448b38ae40430b4252675c
Adam <jamcraftadam@gmail.com>
2016-06-19 15:39:56 +0100

    Added skin randomization

--- resources/Server/resources/fivem-map-hipster/map.lua
+++ resources/Server/resources/fivem-map-hipster/map.lua
@@ -1,5 +1,26 @@
 vehicle_generator "airtug" { -54.26639938354492, -1679.548828125, 28.4414, heading = 228.2736053466797 }
 
 spawnpoint 's_m_y_xmech_02' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'A_F_Y_Hippie_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'A_F_Y_SouCent_02' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'A_F_Y_Vinewood_03' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'A_M_M_Business_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'A_M_M_EastSA_02' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'A_M_M_FatLatin_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'A_M_M_Golfer_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'A_M_M_KTown_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'A_M_Y_Gay_02' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'A_M_Y_VinDouche_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'MP_M_ShopKeep_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'S_F_M_SweatShop_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'S_F_Y_Shop_LOW' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'S_F_Y_Migrant_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'S_F_Y_MovPrem_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'S_M_M_DockWork_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'S_M_M_Gardener_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'S_M_Y_AirWorker' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'S_M_Y_Pilot_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'S_M_Y_XMech_01' { x = -802.311, y = 175.056, z = 72.8446 }
+
 
 --
